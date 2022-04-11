6a670256c5eb09dabb31c7408dbe24e641ec3f39
tooo <toooo123@live.ca>
2022-04-11 14:47:56 -0700

    main with print and running classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream> 
 using namespace std;
 
+#include "PipelineSimulation.h"
+#include "TraceInput.h"
+
 int main (int argc, char* argv[]) {
     // ./proj2 trace_file_name start_inst inst_count W
 
@@ -13,4 +16,16 @@ int main (int argc, char* argv[]) {
     int start_inst = atoi(argv[2]);
     int inst_count = atoi(argv[3]);
     int width = atoi(argv[4]);
+
+    cout << "Simulating Processor Pipeline" << endl;
+    cout << "Trace File Name: " << trace_file_name << endl;
+    printf("start_inst: %d, inst_count: %d, width: %d\n", start_inst, inst_count, width);
+
+    TraceInput* trace_input = new TraceInput(trace_file_name, start_inst, inst_count);
+    PipelineSimulation* pipeline_simulation = new PipelineSimulation(trace_input, width);
+
+    pipeline_simulation->start();
+
+    delete trace_input;
+    delete pipeline_simulation;
 }
