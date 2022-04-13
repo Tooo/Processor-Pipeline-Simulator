6426496a8b4ca864384267b2fd29c5e1134984cd
Dynazyde <dyland7293@gmail.com>
2022-04-12 19:44:54 -0700

    idk why this didnt push

--- TraceInput.cpp
+++ TraceInput.cpp
@@ -1,7 +1,30 @@
 #include "TraceInput.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
 
 TraceInput::TraceInput(string trace_file_name, int start_inst, int inst_count) {
     this->trace_file_name = trace_file_name;
     this->start_inst = start_inst;
     this->inst_count = inst_count;
+
+    prepFile();
+}
+
+int TraceInput::getCurrLine(){
+    return curr_line;
+}
+
+void TraceInput::prepFile(){
+
+    std::ifstream traceFile (trace_file_name);
+
+    if(traceFile.is_open()){
+
+        while(curr_line < start_inst){
+            traceFile.get();
+            curr_line++;
+        }
+    }
 }
