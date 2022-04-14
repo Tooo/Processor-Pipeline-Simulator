efd51b05462959492af96410a15445d281b0124d
tooo <toooo123@live.ca>
2022-04-14 14:37:48 -0700

    make vectors and dequeue public

--- InstructionManager.h
+++ InstructionManager.h
@@ -7,7 +7,7 @@ using namespace std;
 #ifndef PIPELINESIMULATION_H_
 #define PIPELINESIMULATION_H_
 class InstructionManager {
-    private:
+    public:
         deque<Instruction> fetch_queue;
         vector<Instruction> fetch_vector;
 
@@ -22,47 +22,6 @@ class InstructionManager {
 
         deque<Instruction> retire_queue;
         vector<Instruction> retire_vector;
-    
-    public:
-        void enqueueFetch(Instruction instruction);
-        Instruction dequeueFetch();
-        void insertFetch(Instruction instruction);
-        Instruction removeFetch();
-        InstructionType nextTypeFetch() {return fetch_queue.front().type;}
-        bool isFetchQueueEmpty() {return fetch_queue.empty();}
-        bool isFetchEmpty() {return fetch_vector.empty();}
-
-        void enqueueDecode(Instruction instruction);
-        Instruction dequeueDecode();
-        void insertDecode(Instruction instruction);
-        Instruction removeDecode();
-        InstructionType nextTypeDecode() {return decode_queue.front().type;}
-        bool isDecodeQueueEmpty() {return decode_queue.empty();}
-        bool isDecodeEmpty() {return decode_vector.empty();}
-
-        void enqueueExecute(Instruction instruction);
-        Instruction dequeueExecute();
-        void insertExecute(Instruction instruction);
-        Instruction removeExecute();
-        InstructionType nextTypeExecute() {return execute_queue.front().type;}
-        bool isExecuteQueueEmpty() {return execute_queue.empty();}
-        bool isExecuteEmpty() {return execute_vector.empty();}
-
-        void enqueueMemory(Instruction instruction);
-        Instruction dequeueMemory();
-        void insertMemory(Instruction instruction);
-        Instruction removeMemory();
-        InstructionType nextTypeMemory() {return memory_queue.front().type;}
-        bool isMemoryQueueEmpty() {return memory_queue.empty();}
-        bool isMemoryEmpty() {return memory_vector.empty();}
-
-        void enqueueRetire(Instruction instruction);
-        Instruction dequeueRetire();
-        void insertRetire(Instruction instruction);
-        Instruction removeRetire();
-        InstructionType nextTypeRetire() {return retire_queue.front().type;}
-        bool isRetireQueueEmpty() {return retire_queue.empty();}
-        bool isRetireEmpty() {return retire_vector.empty();}
 
 };
 #endif
