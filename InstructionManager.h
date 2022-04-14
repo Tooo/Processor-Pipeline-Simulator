03762982c67e78b9e346b12045cf661faf6f9809
tooo <toooo123@live.ca>
2022-04-13 23:07:15 -0700

    isempty

--- InstructionManager.h
+++ InstructionManager.h
@@ -28,26 +28,40 @@ class InstructionManager {
         Instruction dequeueFetch();
         void insertFetch(Instruction instruction);
         Instruction removeFetch();
+        bool isFetchQueueEmpty() {return fetch_queue.empty();}
+        bool isFetchEmpty() {return fetch_vector.empty();}
 
         void enqueueDecode(Instruction instruction);
+        InstructionType nextTypeDecode();
         Instruction dequeueDecode();
         void insertDecode(Instruction instruction);
         Instruction removeDecode();
+        bool isDecodeQueueEmpty() {return decode_queue.empty();}
+        bool isDecodeEmpty() {return decode_vector.empty();}
 
         void enqueueExecute(Instruction instruction);
+        InstructionType nextTypeExecute();
         Instruction dequeueExecute();
         void insertExecute(Instruction instruction);
         Instruction removeExecute();
+        bool isExecuteQueueEmpty() {return execute_queue.empty();}
+        bool isExecuteEmpty() {return execute_vector.empty();}
 
         void enqueueMemory(Instruction instruction);
+        InstructionType nextTypeMemory();
         Instruction dequeueMemory();
         void insertMemory(Instruction instruction);
         Instruction removeMemory();
+        bool isMemoryQueueEmpty() {return memory_queue.empty();}
+        bool isMemoryEmpty() {return memory_vector.empty();}
 
         void enqueueRetire(Instruction instruction);
+        InstructionType nextTypeRetire();
         Instruction dequeueRetire();
         void insertRetire(Instruction instruction);
         Instruction removeRetire();
+        bool isRetireQueueEmpty() {return memory_queue.empty();}
+        bool isRetireEmpty() {return memory_vector.empty();}
 
 };
 #endif
