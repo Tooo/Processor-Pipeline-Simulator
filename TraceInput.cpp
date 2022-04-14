fbc34f9099e63eeda59d3020624404e008e54ff2
tooo <toooo123@live.ca>
2022-04-14 13:15:06 -0700

    fix starting line

--- TraceInput.cpp
+++ TraceInput.cpp
@@ -5,6 +5,7 @@ TraceInput::TraceInput(string trace_file_name, int start_inst, int inst_count) {
     this->trace_file_name = trace_file_name;
     this->start_inst = start_inst;
     this->inst_count = inst_count;
+    curr_line = 1;
 
     prepFile();
 }
